62046ab4d51babfa6ae7e66c7a7e3277b99296c6
luc0liv <luoliveiradacunha@gmail.com>
2023-08-31 23:48:02 -0300

    feat: cria classes varejo e empreiteira que herdam de Empresa e implementam IEmpresa

---
+++ ExercicioDoisInterface/Empreiteira.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioDoisInterface
+{
+    internal class Empreiteira : Empresa, IEmpresa
+    {
+        public Empreiteira(string nomeFantasia, string razaoSocial, string cNPJ, string setor) : base(nomeFantasia, razaoSocial, cNPJ, setor)
+        {
+        }
+
+        public void ImprimeInfo()
+        {
+            Console.WriteLine($"Nome Fantasia: {NomeFantasia}");
+            Console.WriteLine($"Razão Social: {RazaoSocial}");
+            Console.WriteLine($"CNPJ: {CNPJ}");
+            Console.WriteLine($"Setor: {Setor}");
+        }
+
+        public void RealizarVenda()
+        {
+            Console.WriteLine("Venda de Serviços realizada!");
+        }
+    }
+}
---
+++ ExercicioDoisInterface/Varejo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioDoisInterface
+{
+    internal class Varejo : Empresa, IEmpresa
+    {
+        public Varejo(string nomeFantasia, string razaoSocial, string cNPJ, string setor) : base(nomeFantasia, razaoSocial, cNPJ, setor)
+        {
+        }
+
+        public void ImprimeInfo()
+        {
+            Console.WriteLine($"Nome Fantasia: {NomeFantasia}");
+            Console.WriteLine($"Razão Social: {RazaoSocial}");
+            Console.WriteLine($"CNPJ: {CNPJ}");
+            Console.WriteLine($"Setor: {Setor}");
+        }
+
+        public void RealizarVenda()
+        {
+            Console.WriteLine("Venda de PRODUTOS realizada!");
+        }
+    }
+}
