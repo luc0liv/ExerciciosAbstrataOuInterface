bd1a9471d926449d71cb2f5eb96d5686525162af
luc0liv <luoliveiradacunha@gmail.com>
2023-08-31 23:32:02 -0300

    feat: adiciona classe abstrata Empresa

---
+++ ExercicioUmAbstrata/Empresa.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioUmAbstrata
+{
+    public abstract class Empresa
+    {
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+        public string CNPJ { get; set; }
+        public string Setor { get; set; }
+
+        public Empresa(string nomeFantasia, string razaoSocial, string cNPJ, string setor)
+        {
+            NomeFantasia = nomeFantasia ?? throw new ArgumentNullException(nameof(nomeFantasia));
+            RazaoSocial = razaoSocial ?? throw new ArgumentNullException(nameof(razaoSocial));
+            CNPJ = cNPJ ?? throw new ArgumentNullException(nameof(cNPJ));
+            Setor = setor ?? throw new ArgumentNullException(nameof(setor));
+        }
+
+        public abstract void ImprimeInfo();
+        public abstract void RealizarVenda();
+    }
+}
+
