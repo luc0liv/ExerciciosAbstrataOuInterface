015c76b1e48353738d7d8067a00380d01d67c094
luc0liv <luoliveiradacunha@gmail.com>
2023-08-31 23:33:47 -0300

    feat: adiciona classe Varejo que herda de Empresa

---
+++ ExercicioUmAbstrata/Varejo.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioUmAbstrata
+{
+    internal class Varejo : Empresa
+    {
+        public Varejo(string nomeFantasia, string razaoSocial, string cNPJ, string setor) : base(nomeFantasia, razaoSocial, cNPJ, setor)
+        {
+        }
+
+        public override void ImprimeInfo()
+        {
+            Console.WriteLine($"Nome Fantasia: {NomeFantasia}");
+            Console.WriteLine($"Razão Social: {RazaoSocial}");
+            Console.WriteLine($"CNPJ: {CNPJ}");
+            Console.WriteLine($"Setor: {Setor}");
+        }
+
+        public override void RealizarVenda()
+        {
+            Console.WriteLine("Venda de PRODUTOS realizada!");
+        }
+    }
+}
