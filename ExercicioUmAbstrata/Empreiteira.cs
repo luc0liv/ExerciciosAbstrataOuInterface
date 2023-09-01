c3e47655178ca82ab8ef4a41188b87170e07d84b
luc0liv <luoliveiradacunha@gmail.com>
2023-08-31 23:35:15 -0300

    feat: adiciona Classe Empreiteira que herda de Empresa

---
+++ ExercicioUmAbstrata/Empreiteira.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioUmAbstrata
+{
+    internal class Empreiteira : Empresa
+    {
+        public Empreiteira(string nomeFantasia, string razaoSocial, string cNPJ, string setor) : base(nomeFantasia, razaoSocial, cNPJ, setor)
+        {
+        }
+
+        public override void ImprimeInfo()
+        {
+            Console.WriteLine($"Nome Fantasia: {NomeFantasia}");
+            Console.WriteLine($"Razão Social: {RazaoSocial}");
+            Console.WriteLine($"CNPJ: {CNPJ}");
+            Console.WriteLine($"Setor: {Setor}");
+        }
+
+        public override void RealizarVenda()
+        {
+            Console.WriteLine("Venda de Serviços realizada!");
+        }
+    }
+}
