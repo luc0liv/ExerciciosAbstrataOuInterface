3c5384aa40ddc1cafceab60b4cd85e39cf066355
luc0liv <luoliveiradacunha@gmail.com>
2023-08-31 23:37:57 -0300

    feat: cria lista de empresas abstratas

--- ExercicioUmAbstrata/Program.cs
+++ ExercicioUmAbstrata/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Empresa> listaEmpresas = new List<Empresa>();
+
+            Varejo varejo = new("Varejão", "Varejão LTDA", "1234567", "Varejo");
+
+            Empreiteira empreiteira = new("Empreiteirona", "Empreiteira da panterona LTDA", "2345321", "Construção Civil");
+
+            listaEmpresas.Add(varejo);
+            listaEmpresas.Add(empreiteira);
+
+            Console.WriteLine("Exercício 1 - Classes abstratas");
+
+            listaEmpresas.ForEach(empresa =>
+            {
+                empresa.ImprimeInfo();
+                empresa.RealizarVenda();
+                Console.WriteLine();
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine();
+            });
         }
     }
 }
